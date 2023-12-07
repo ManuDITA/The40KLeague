461e55b6f384547760ad021a714cb11b516c4691
Emanuele Simeone <68699073+ManuDITA@users.noreply.github.com>
2023-12-07 16:44:15 +0100

    Added previously not present aws-export and amplifyconfig

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,14 @@
+const awsExports = {
+    "REGION" : 'eu-west-3',
+    "USER_POOL_ID" : 'eu-west-3_qD5XkrxP0',
+    "USER_POOL_CLIENT_ID" : '7nhk4t7es6v27v9r8m9i7g8ut9'
+};
+
+const APIs = {
+    "getSeasonAPI" : "",
+    "getUser" : "https://iw5168sfve.execute-api.eu-west-3.amazonaws.com/dev/user"
+}
+
+//string
+
+export default awsExports;
